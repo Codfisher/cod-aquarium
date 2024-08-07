e5561eccd727ee97fd250c88cd5600886fb894d3
Codfisher <pony2140@livemail.tw>
2024-08-08 02:56:26 +0800

    init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 export default defineNuxtConfig({
   extends: 'content-wind',
+
   routeRules: {
     '/': { prerender: true }
-  }
+  },
+
+  compatibilityDate: '2024-08-08',
+  modules: ["@nuxt/image"]
 })
