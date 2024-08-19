fe5d4a5c7bbda6bc248e849af6913b6cd1578d0f
Codfisher <pony2140@livemail.tw>
2024-08-19 14:03:48 +0800

    feat: 成功使用 article-overview layout

---
+++ layouts/article-overview.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class=" flex flex-col items-center py-10">
+    <div class=" w-1/2">
+      <h1>文章列表</h1>
+
+      <p>Some default layout content shared across all pages</p>
+
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
