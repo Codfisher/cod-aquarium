36eaa8e29e5a97d6014a8a2bc92d8284343f30e4
bgm.cod <bgm.codfish@gmail.com>
2024-08-20 17:13:28 +0800

    docs: 更新首頁內容

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,11 @@ export default defineNuxtConfig({
   extends: '@nuxt-themes/docus',
 
   compatibilityDate: '2024-08-08',
-  modules: ['@vueuse/nuxt', '@nuxtjs/tailwindcss', '@nuxt/image'],
+  modules: [
+    '@vueuse/nuxt',
+    '@nuxtjs/tailwindcss',
+    '@nuxt/image'
+  ],
 
   css: [
     '/styles/main.sass',
