63768652b44e88133a25389902a6efed60e9ea34
bgm.cod <bgm.codfish@gmail.com>
2024-12-05 15:42:21 +0800

    Update donate-section.vue

--- components/donate-section.vue
+++ components/donate-section.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="p-4 border rounded-xl mb-10 vp-doc ">
-    如果您覺得我的文章或作品對您有幫助，
+    如果我的文章對您有幫助，
     <a
       href="https://portaly.cc/codfish/support"
       target="_blank"
     >
       歡迎贊助我一杯咖啡
-    </a>！(´▽`ʃ💖ƪ)
+    </a>，鱈魚感謝您！(´▽`ʃ💖ƪ)
   </div>
 </template>
 
