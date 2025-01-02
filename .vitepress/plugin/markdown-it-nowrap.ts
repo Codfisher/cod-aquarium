9a5fbbe7298d0df152f3b227711274f0cd9b4701
bgm.cod <bgm.codfish@gmail.com>
2025-01-02 11:17:57 +0800

    feat: 完成 markdown-it-nowrap

--- .vitepress/plugin/markdown-it-nowrap.ts
+++ .vitepress/plugin/markdown-it-nowrap.ts
@@ -5,13 +5,13 @@ type RuleInline = Parameters<
   MarkdownIt['inline']['ruler']['before']
 >[2]
 
-/**
+/** 使用 % 包圍文字，即可建立不換行的元素
  * https://vitepress.dev/guide/markdown#advanced-configuration
  *
  * @param md
  */
 export function markdownItNowrap(md: MarkdownIt) {
-  // 定義內聯規則
+  /** 解析 % 區塊 */
   const parsePercentSyntax: RuleInline = (state, silent) => {
     const start = state.pos
     const marker = '%'
@@ -23,25 +23,25 @@ export function markdownItNowrap(md: MarkdownIt) {
 
     const end = state.src.indexOf(marker, start + 1)
     if (end === -1) {
-      return false // 如果找不到結尾的 %，跳過
+      return false
     }
 
     if (!silent) {
-      const token = state.push('percent_syntax', '', 0)
+      const token = state.push('nowrap_span', '', 0)
       token.content = state.src.slice(start + 1, end).trim()
     }
 
-    state.pos = end + 1 // 更新解析位置
+    state.pos = end + 1
     return true
   }
 
-  // Token 渲染器
+  /** 渲染 nowrap_span 區塊內容 */
   function renderPercentSyntax(tokens: Token[], idx: number) {
     if (!tokens[idx]?.content) {
       return ''
     }
 
-    return tokens[idx].content
+    return `<span class="text-nowrap">${tokens[idx].content}</span>`
   }
 
   // 插件註冊
