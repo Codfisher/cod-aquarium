a6ac84264c72cad550223df27fa22f86717e60c7
Codfisher <pony2140@livemail.tw>
2025-01-09 00:47:55 +0800

    feat: 開啟陰影

--- content/aquarium/sound-blocks/blocks/tree.ts
+++ content/aquarium/sound-blocks/blocks/tree.ts
@@ -20,7 +20,7 @@ export async function createTreeBlock(
     SceneLoader.ImportMeshAsync(
       '',
       '/sound-blocks/hexagon-pack/decoration/nature/',
-      'trees_B_large.gltf',
+      'trees_B_medium.gltf',
       scene,
     ),
     SceneLoader.ImportMeshAsync(
@@ -37,10 +37,18 @@ export async function createTreeBlock(
     meshes.push(treeResult.meshes[0])
   }
 
+  treeResult.meshes.forEach((mesh) => {
+    mesh.receiveShadows = true
+  })
+
   if (hexResult.meshes[0]) {
     hexResult.meshes[0].position = new Vector3(0, 0.5, 0)
     meshes.push(hexResult.meshes[0])
   }
+
+  hexResult.meshes.forEach((mesh) => {
+    mesh.receiveShadows = true
+  })
 
   return {
     meshes,
