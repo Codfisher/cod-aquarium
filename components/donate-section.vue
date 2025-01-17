09d7689fa1ab4e37fdcc83603085b52dd53d9e18
bgm.cod <bgm.codfish@gmail.com>
2025-01-17 13:43:30 +0800

    fix: 修復 window 不存在問題

--- components/donate-section.vue
+++ components/donate-section.vue
@@ -1,32 +1,42 @@
 <template>
-  <div class="p-4 border rounded-xl mb-10 vp-doc">
-    如果我的文章對您有所啟發或幫助，歡迎「
-    <a
-      href="https://portaly.cc/codfish/support"
-      target="_blank"
-    >
-      請我喝一杯咖啡
-    </a>
+  <ClientOnly>
+    <div class="p-4 border rounded-xl mb-10 vp-doc">
+      如果我的文章對您有所啟發或幫助，歡迎「
+      <a
+        href="https://portaly.cc/codfish/support"
+        target="_blank"
+      >
+        請我喝一杯咖啡
+      </a>
 
-    」或花 10 秒登入 LikeCoin 按下方按鈕拍手鼓勵我喔！
-    <iframe
-      :key="src"
-      scrolling="no"
-      frameborder="0"
-      :src
-    />
+      」或花 10 秒登入 LikeCoin 按下方按鈕拍手鼓勵我喔！
+      <iframe
+        :key="src"
+        scrolling="no"
+        frameborder="0"
+        :src
+      />
 
-    <div class=" text-center p-4 bg-slate-50 rounded-xl text-lg text-gray-600">
-      鱈魚感謝您！ <span class="text-nowrap">(´▽`ʃ💖ƪ)</span>
+      <div class=" text-center p-4 bg-slate-50 rounded-xl text-lg text-gray-600">
+        鱈魚感謝您！ <span class="text-nowrap">(´▽`ʃ💖ƪ)</span>
+      </div>
     </div>
-  </div>
+  </ClientOnly>
 </template>
 
 <script setup lang="ts">
+import { pipe } from 'remeda'
+import { useRoute } from 'vitepress'
 import { computed } from 'vue'
 
+const route = useRoute()
+
 const src = computed(() => {
-  const referrer = encodeURIComponent(window.location.href.replace('.html', ''))
+  const referrer = pipe(
+    window?.location?.href ?? route.path,
+    (value) => encodeURIComponent(value.replace('.html', '')),
+  )
+
   return `https://button.like.co/in/embed/codlin/button?referrer=${referrer}`
 })
 </script>
