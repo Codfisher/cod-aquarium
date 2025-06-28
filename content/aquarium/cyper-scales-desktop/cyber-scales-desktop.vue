4fc17072287e7a0bcde51abc0ecc9b61f4ac33ae
Codfisher <pony2140@livemail.tw>
2025-06-28 23:56:19 +0800

    feat: 新增 aquarium\cyper-scales-desktop

---
+++ content/aquarium/cyper-scales-desktop/cyber-scales-desktop.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class=" overflow-hidden">
+    安安
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="sass">
+body.overflow-hidden
+  overflow: hidden
+</style>
+
+<style lang="sass" scoped>
+.canvas
+  outline: none
+</style>
