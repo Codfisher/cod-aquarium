bd60bf282cf1d1b7975bbf47ca10f293bab26eb2
bgm.cod <bgm.codfish@gmail.com>
2025-08-26 14:49:14 +0800

    docs

---
+++ content/blog-ocean-world/js-highlight-api/highlight-color.vue
@@ -0,0 +1,37 @@
+<template>
+  <div ref="tableRef">
+    <div class=" flex items-center gap-1 border rounded p-2 mb-2">
+      <label>背景顏色</label>
+      <input
+        type="color"
+        v-model="style.backgroundColor"
+      />
+      <label class="ml-2">文字顏色</label>
+      <input
+        type="color"
+        v-model="style.color"
+      />
+    </div>
+
+    <filter-table v-model="keyword" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, useTemplateRef } from 'vue'
+import FilterTable from './filter-table.vue';
+import { useTextHighlight } from '../../../composables/use-text-highlight';
+
+const tableRef = useTemplateRef('tableRef')
+
+const keyword = ref('')
+const style = ref({
+  backgroundColor: '#ffff00',
+  color: '#000000',
+})
+useTextHighlight(keyword, {
+  target: tableRef,
+  background: () => style.value.backgroundColor,
+  color: () => style.value.color,
+})
+</script>
--- content/blog-ocean-world/js-highlight-api/highlight-table.vue
+++ content/blog-ocean-world/js-highlight-api/highlight-table.vue
@@ -1,18 +1,5 @@
 <template>
   <div ref="tableRef">
-    <div class=" flex items-center gap-1 border rounded p-2 mb-2">
-      <label>背景顏色</label>
-      <input
-        type="color"
-        v-model="style.backgroundColor"
-      />
-      <label class="ml-2">文字顏色</label>
-      <input
-        type="color"
-        v-model="style.color"
-      />
-    </div>
-
     <filter-table v-model="keyword" />
   </div>
 </template>
@@ -25,13 +12,7 @@ import { useTextHighlight } from '../../../composables/use-text-highlight';
 const tableRef = useTemplateRef('tableRef')
 
 const keyword = ref('')
-const style = ref({
-  backgroundColor: '#ffff00',
-  color: '#000000',
-})
 useTextHighlight(keyword, {
   target: tableRef,
-  background: () => style.value.backgroundColor,
-  color: () => style.value.color,
 })
 </script>
