b98bc857b7d5f27f210764360ebd6de0386254b5
bgm.cod <bgm.codfish@gmail.com>
2025-08-29 13:30:57 +0800

    fix: 移除 base-window 內部 div 的背景顏色以改善視覺效果

--- content/aquarium/cyber-scales-desktop/cyber-scales-desktop.vue
+++ content/aquarium/cyber-scales-desktop/cyber-scales-desktop.vue
@@ -1,7 +1,7 @@
 <template>
   <div class=" w-screen h-screen flex justify-center items-center">
     <base-window title="title 標題">
-      <div class="bg-white p-6">
+      <div class="p-6">
         安安
       </div>
     </base-window>
