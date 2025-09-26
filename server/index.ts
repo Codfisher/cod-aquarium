115326f751fc529eda5c2ec8ec217bae54d82e59
codlin <pony2140@livemail.tw>
2025-09-26 05:40:14 +0000

    feat: 新增 @hono/zod-validator 以增強 API 請求驗證，並更新相關依賴

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,8 @@
 import type { Env } from './type'
+import { zValidator } from '@hono/zod-validator'
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
+import { z } from 'zod'
 
 const app = new Hono<Env>()
 
@@ -17,9 +19,23 @@ app.use('/api/*', async (ctx, next) => {
   await next()
 })
 
+const reactionsApi = new Hono<Env>().basePath('/api/reactions')
+
 /** 取得文章有多少響應 */
-app.get('/api/reactions', (ctx) => {
-  return ctx.text('Hello Hono!')
-})
+reactionsApi.get(
+  '/',
+  zValidator(
+    'query',
+    z.object({
+      articleId: z.string().refine(
+        (value) => value.includes('blog-') || value.includes('column-'),
+        { message: '文章 ID 無效' },
+      ),
+    }),
+  ),
+  (ctx) => {
+    return ctx.text('Hello Hono!')
+  },
+)
 
 export default app
