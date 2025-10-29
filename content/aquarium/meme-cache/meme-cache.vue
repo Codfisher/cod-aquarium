0eafe4ee3ae583b614aa9e4db21be163118639ef
Codfisher <pony2140@livemail.tw>
2025-10-29 15:05:28 +0800

    chore

--- content/aquarium/meme-cache/meme-cache.vue
+++ content/aquarium/meme-cache/meme-cache.vue
@@ -177,7 +177,7 @@ import ImgList from './components/img-list.vue'
 import { useMemeData } from './composables/use-meme-data'
 import { useStickyToolbar } from './composables/use-sticky-toolbar'
 
-const version = '0.3.0'
+const version = '0.3.1'
 // onMounted(() => {
 //   document.title = pipe(
 //     document.title.split('v'),
