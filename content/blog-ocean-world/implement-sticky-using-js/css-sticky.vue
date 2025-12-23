93e18faa5a5df556de4826738689276a86946b48
Codfisher <pony2140@livemail.tw>
2025-12-23 16:49:45 +0800

    feat: add results section and improve layout in sticky positioning examples for better clarity and usability

--- content/blog-ocean-world/implement-sticky-using-js/css-sticky.vue
+++ content/blog-ocean-world/implement-sticky-using-js/css-sticky.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="h-[20vh] overflow-y-auto p-10 rounded-lg border-3 border-gray-200">
-    <div class="flex items-start h-screen relative">
+    <div class="flex items-start h-[40vh] relative">
       <div class="h-[15vh] relative text-center top-0 p-4 rounded-lg bg-sky-200 text-sky-700 font-semibold">
         relative
       </div>
--- content/blog-ocean-world/implement-sticky-using-js/use-v-sticky.vue
+++ content/blog-ocean-world/implement-sticky-using-js/use-v-sticky.vue
@@ -3,11 +3,13 @@
     <div class="h-[25vh] card border-gray-200">
       <div class="h-[30vh] card border-gray-300">
         <div class="h-[35vh] card border-gray-400">
-          <div
-            v-sticky
-            class="sticky top-0 text-center p-4 rounded-lg bg-blue-400 text-white"
-          >
-            我是內容 ヽ(́◕◞౪◟◕‵)ﾉ
+          <div class="h-[40vh] card border-gray-500">
+            <div
+              v-sticky
+              class="sticky top-0 text-center p-4 rounded-lg bg-blue-400 text-white"
+            >
+              我是內容 ヽ(́◕◞౪◟◕‵)ﾉ
+            </div>
           </div>
         </div>
       </div>
