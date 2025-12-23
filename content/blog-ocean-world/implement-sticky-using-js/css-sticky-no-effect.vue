ee5b1e56872452c736dd4f6fdff79a2dffefdd3e
Codfisher <pony2140@livemail.tw>
2025-12-23 13:23:24 +0800

    feat: add JavaScript examples and enhance explanation of sticky positioning in blog post

---
+++ content/blog-ocean-world/implement-sticky-using-js/css-sticky-no-effect.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex items-start h-[30vh] overflow-y-auto p-10 rounded-lg border-3 border-gray-200">
+    <div class=" w-full h-[40vh] overflow-y-auto p-10 rounded-lg border-3 border-gray-200">
+      <div class="h-[50vh] overflow-y-auto bg-blue-200 rounded-lg">
+        <div class=" sticky top-0 p-4 rounded-lg  text-blue-500 font-semibold">
+          無法 sticky ( ´•̥̥̥ ω •̥̥̥` )
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ content/blog-ocean-world/implement-sticky-using-js/css-sticky.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex items-start h-[30vh] overflow-y-auto p-10 rounded-lg border-3 border-gray-200">
+    <div class="h-screen relative">
+      <div class="relative top-0 p-4 rounded-lg bg-blue-300 text-blue-700 font-semibold">
+        relative
+      </div>
+
+      <div class=" absolute top-0 left-1/1 p-4 rounded-lg bg-green-300 text-green-700 font-semibold">
+        absolute
+      </div>
+
+      <div class="sticky top-0 p-4 rounded-lg bg-red-300 text-red-700 font-semibold">
+        sticky
+      </div>
+    </div>
+  </div>
+</template>
