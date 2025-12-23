c6b3e931b591c5e04ac73111b2096352da5b62ee
Codfisher <pony2140@livemail.tw>
2025-12-23 15:51:12 +0800

    style: update background colors and text styles for improved visual consistency in sticky positioning examples

--- content/blog-ocean-world/implement-sticky-using-js/css-sticky-no-effect.vue
+++ content/blog-ocean-world/implement-sticky-using-js/css-sticky-no-effect.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex items-start h-[20vh] overflow-y-auto p-14 pl-6 rounded-lg border-3 border-gray-200">
     <div class=" w-full h-[25vh] overflow-y-auto p-14 pl-6 rounded-lg border-3 border-gray-200">
-      <div class="h-[30vh] overflow-y-auto bg-blue-200 rounded-lg">
-        <div class=" sticky top-0 p-4 rounded-lg  text-blue-500 font-semibold">
+      <div class="h-[30vh] overflow-y-auto bg-sky-200 rounded-lg">
+        <div class=" sticky top-0 p-4 rounded-lg  text-sky-700 font-semibold">
           滾動外層就看不見惹 ( ´•̥̥̥ ω •̥̥̥` )
         </div>
       </div>
--- content/blog-ocean-world/implement-sticky-using-js/css-sticky.vue
+++ content/blog-ocean-world/implement-sticky-using-js/css-sticky.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="h-[20vh] overflow-y-auto p-10 rounded-lg border-3 border-gray-200">
     <div class="flex items-start h-screen relative">
-      <div class="h-[15vh] relative text-center top-0 p-4 rounded-lg bg-sky-300 text-sky-700 font-semibold">
+      <div class="h-[15vh] relative text-center top-0 p-4 rounded-lg bg-sky-200 text-sky-700 font-semibold">
         relative
       </div>
 
-      <div class="h-[15vh] absolute text-center top-0 left-0 translate-x-2/1 p-4 rounded-lg bg-emerald-300 text-emerald-700 font-semibold">
+      <div class="h-[15vh] absolute text-center top-0 left-0 translate-x-2/1 p-4 rounded-lg bg-emerald-200 text-emerald-700 font-semibold">
         absolute
       </div>
 
-      <div class=" h-[15vh] rounded-lg bg-rose-300 text-rose-700 font-semibold">
+      <div class=" h-[15vh] rounded-lg bg-rose-200 text-rose-700 font-semibold">
         <div class="sticky text-center top-0 p-4 font-semibold">
           sticky
         </div>
