4f164ba86f3026163b86c6bec2316a2d0f608bb9
Codfisher <pony2140@livemail.tw>
2026-01-12 17:23:17 +0800

    fix: improve error handling in v-sticky directive to enhance stability and user experience

---
+++ content/blog-ocean-world/detached-elements/detached-elements.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    v-bind="containerProps"
+    class="h-[50vh] overflow-auto p-2 border rounded"
+  >
+    <div v-bind="wrapperProps">
+      <div
+        v-for="{ index } in list"
+        :key="index"
+        class="border mb-2 flex items-center justify-center h-[40px]"
+      >
+        Row {{ index }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useVirtualList } from '@vueuse/core'
+
+const allItems = Array.from(Array.from({ length: 99999 })
+  .keys())
+
+const { list, containerProps, wrapperProps } = useVirtualList(
+  allItems,
+  { itemHeight: 40 },
+)
+</script>
