e7d2b744498b78be1e6935934a5b4f40c7af614d
Codfisher <pony2140@livemail.tw>
2026-01-27 10:25:09 +0800

    feat

--- content/aquarium/codstack/codstack.vue
+++ content/aquarium/codstack/codstack.vue
@@ -18,19 +18,47 @@
         <sense-viewer class="w-full h-full" />
       </u-dashboard-group>
 
-      <div class=" fixed right-0 top-0 p-2 px-3 text-xs opacity-10">
-        {{ version }}
+      <div class="font-orbitron fixed right-0 bottom-0 p-2 px-3 opacity-50">
+        <span class="text-base">CodStack</span>
+
+        <span class="ml-1 text-xs text-gray-500">
+          v{{ version }}
+        </span>
       </div>
     </u-app>
   </client-only>
 </template>
 
 <script setup lang="ts">
+import { onBeforeUnmount, onMounted } from 'vue'
 import FilePreviewPanel from './components/file-preview-panel.vue'
 import SenseViewer from './components/sense-viewer.vue'
+
+// 載入字體
+const fontHref = 'https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&family=Orbitron:wght@400..900'
+let linkEl: HTMLLinkElement
+onMounted(() => {
+  // 已經有同樣的 link 就不要重複
+  const existed = Array.from(document.head.querySelectorAll('link'))
+    .find((link) => link.getAttribute('href') === fontHref)
+  if (existed)
+    return
+
+  linkEl = document.createElement('link')
+  linkEl.rel = 'stylesheet'
+  linkEl.href = fontHref
+  document.head.appendChild(linkEl)
+})
+
+onBeforeUnmount(() => {
+  if (linkEl)
+    document.head.removeChild(linkEl)
+})
 
 const version = '0.1.0'
 </script>
 
 <style lang="sass" scoped>
+.font-orbitron
+  font-family: "Orbitron", sans-serif
 </style>
