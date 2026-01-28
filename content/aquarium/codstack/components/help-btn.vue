ba762628a9cbda7fa7e016c4e649de39e7a63b54
Codfisher <pony2140@livemail.tw>
2026-01-28 17:32:29 +0800

    feat: add help button Vue component

--- content/aquarium/codstack/components/help-btn.vue
+++ content/aquarium/codstack/components/help-btn.vue
@@ -14,9 +14,12 @@
           <li><u-kbd>Esc</u-kbd>：關閉、取消選取</li>
           <li><u-kbd>Ctrl</u-kbd> + <u-kbd>Z</u-kbd>：上一步</li>
           <li><u-kbd>Ctrl</u-kbd> + <u-kbd>Y</u-kbd>：下一步</li>
-          <li>選取後 + <u-kbd>G</u-kbd>：移動</li>
-          <li>選取後 + <u-kbd>S</u-kbd>：縮放</li>
-          <li>選取後 + <u-kbd>R</u-kbd>：旋轉</li>
+          <li>選取後按 <u-kbd>G</u-kbd>：移動</li>
+          <li>選取後按 <u-kbd>S</u-kbd>：縮放</li>
+          <li>選取後按 <u-kbd>R</u-kbd>：旋轉</li>
+          <li>移動、縮放、旋轉時 + <u-kbd>Shift</u-kbd>：0.1 單位或 1 度步長</li>
+          <li>移動、縮放、旋轉時 + <u-kbd>Alt</u-kbd>：自由步長</li>
+          <li>按住 <u-kbd>Shift</u-kbd>選擇：多選</li>
         </ul>
       </div>
     </template>
