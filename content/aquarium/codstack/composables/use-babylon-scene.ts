85713ff55331965e4c00bfa1f5b49f11ba5d59de
Codfisher <pony2140@livemail.tw>
2026-02-01 10:47:34 +0800

    feat: add `useBabylonScene` composable to initialize and configure a Babylon.js engine, scene, and camera.

--- content/aquarium/codstack/composables/use-babylon-scene.ts
+++ content/aquarium/codstack/composables/use-babylon-scene.ts
@@ -8,6 +8,7 @@ import {
   Color4,
   Engine,
   HemisphericLight,
+  KeyboardEventTypes,
   Scene,
   Vector3,
   WebGPUEngine,
@@ -85,6 +86,35 @@ const defaultParam: Required<UseBabylonSceneParam> = {
     camera.wheelDeltaPercentage = 0.01
     camera.lowerRadiusLimit = 5
     camera.upperRadiusLimit = 50
+
+    const mouseWheelInput = camera.inputs.attached.mousewheel
+
+    // 按住 ctrl 時，暫停滾輪縮放
+    scene.onKeyboardObservable.add((kbInfo) => {
+      switch (kbInfo.type) {
+        case KeyboardEventTypes.KEYDOWN:
+          // 當按下 Ctrl 鍵時
+          if (kbInfo.event.key === 'Control' || kbInfo.event.ctrlKey) {
+            mouseWheelInput?.detachControl()
+          }
+          break
+
+        case KeyboardEventTypes.KEYUP:
+          // 當放開 Ctrl 鍵時
+          if (kbInfo.event.key === 'Control' || !kbInfo.event.ctrlKey) {
+            mouseWheelInput?.attachControl(false)
+          }
+          break
+      }
+    })
+
+    useEventListener(canvas, 'wheel', (e) => {
+      // 阻止網頁縮放
+      if (e.ctrlKey) {
+        e.preventDefault()
+        e.stopPropagation()
+      }
+    }, { passive: false })
 
     // 限制鏡頭角度
     camera.lowerBetaLimit = 0
