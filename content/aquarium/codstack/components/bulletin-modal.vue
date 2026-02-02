ef14cb6c29408e3c99f860ce6220d9d050075c5e
Codfisher <pony2140@livemail.tw>
2026-02-02 15:28:45 +0800

    feat: Create BulletinModal Vue component for displaying welcome messages.

---
+++ content/aquarium/codstack/components/bulletin-modal.vue
@@ -0,0 +1,25 @@
+<template>
+  <u-modal title="Help">
+    <slot />
+
+    <template #body>
+      <div class="[&>h2:not(:first-child)]:mt-3">
+        <h2 class="text-lg font-bold ">
+          Welcome to CodStack!
+        </h2>
+      </div>
+    </template>
+  </u-modal>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  label?: string;
+}
+const props = withDefaults(defineProps<Props>(), {
+  label: '',
+})
+</script>
+
+<style scoped lang="sass">
+</style>
