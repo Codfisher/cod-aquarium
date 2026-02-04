451a0bc2c825dd94fd86d125a8c4051cd1dfebe4
Codfisher <pony2140@livemail.tw>
2026-02-05 01:37:44 +0800

    feat: add SceneViewer component for interactive 3D scene rendering and mesh manipulation.

--- content/aquarium/codstack/utils/babylon.ts
+++ content/aquarium/codstack/utils/babylon.ts
@@ -16,58 +16,6 @@ export function getMeshMeta<Meta = MeshMeta>(mesh: AbstractMesh) {
   return mesh.metadata as Meta | undefined
 }
 
-/** 將物體吸附在表面上 */
-export function snapMeshToSurface(
-  mesh: AbstractMesh,
-  pickInfo: PickingInfo,
-  options?: {
-    /** 是否要更新 mesh 的旋轉
-     * @default false
-     */
-    updateRotation?: boolean;
-  },
-) {
-  const updateRotation = options?.updateRotation ?? false
-
-  if (!pickInfo.pickedPoint)
-    return
-
-  const normal = pickInfo.getNormal(true)
-  const point = pickInfo.pickedPoint
-
-  if (!normal)
-    return
-
-  // gltf 會在 Y 軸多 180 度旋轉，需要修正
-  const fixRotation = Quaternion.RotationAxis(Vector3.Up(), Math.PI)
-  if (updateRotation) {
-    // 讓物體的上方 (Y) 對齊表面的法向量
-    const rotationQuaternion = new Quaternion()
-
-    Quaternion.FromUnitVectorsToRef(Vector3.Up(), normal, rotationQuaternion)
-      .multiplyInPlace(fixRotation)
-
-    mesh.rotationQuaternion = rotationQuaternion
-  }
-  else {
-    mesh.rotationQuaternion = Quaternion.Identity()
-      .multiplyInPlace(fixRotation)
-  }
-
-  // 強制更新矩陣
-  mesh.computeWorldMatrix(true)
-
-  // 取得 Local Space 的邊界框上限 (假設 Pivot 在中心，這代表從中心到頂部的距離)
-  const localMaximum = mesh.getBoundingInfo().maximum
-
-  // 計算物體中心到邊緣的距離 (Local Y * Scaling Y)
-  // 修正點：這裡變數名稱改為 distToCenter
-  const distToCenter = localMaximum.y * mesh.scaling.y
-
-  // 計算目標位置：撞擊點 + 沿著法線推出的距離
-  return point.add(normal.scale(distToCenter))
-}
-
 /**
  * 計算 Mesh 貼合表面的變換資訊，但不直接修改 Mesh
  */
