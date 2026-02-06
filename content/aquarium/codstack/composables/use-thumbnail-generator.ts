56fc77fdfb1c5b2435926522b316bca4108227cc
Codfisher <pony2140@livemail.tw>
2026-02-06 09:54:31 +0800

    perf: useThumbnailGenerator 加入並發產生

--- content/aquarium/codstack/composables/use-thumbnail-generator.ts
+++ content/aquarium/codstack/composables/use-thumbnail-generator.ts
@@ -16,106 +16,118 @@ import PQueue from 'p-queue'
 import { pipe } from 'remeda'
 import { getFileFromPath } from '../utils/fs'
 
+/** 注意：同時開太多 canvas 會導致 WebGPU 的記憶體耗盡 */
+const concurrency = 2
+
 function _useThumbnailGenerator(rootFsHandle: FileSystemDirectoryHandle) {
-  const queue = new PQueue({ concurrency: 1 })
+  const queue = new PQueue({ concurrency })
 
-  let _canvas: HTMLCanvasElement | undefined
-  let _engine: Engine | undefined
-  let _scene: Scene | undefined
-  let _camera: ArcRotateCamera | undefined
-
-  let lastContainer: AssetContainer | undefined
+  /** 指派 index */
+  let index = 0
+  const generatorList = Array.from({ length: concurrency }, () => createGenerator())
   const blobUrlList: string[] = []
 
   tryOnScopeDispose(() => {
     blobUrlList.forEach((url) => URL.revokeObjectURL(url))
+    generatorList.forEach((g) => g.dispose())
   })
 
-  function init() {
-    if (_scene) {
-      return
+  function createGenerator() {
+    let lastContainer: AssetContainer | undefined
+
+    const canvas = document.createElement('canvas')
+    canvas.style.visibility = 'hidden'
+    canvas.width = 256
+    canvas.height = 256
+    document.body.appendChild(canvas)
+
+    const engine = new Engine(canvas, true)
+    const scene = new Scene(engine)
+    scene.clearColor = new Color4(0, 0, 0, 0)
+
+    const camera = new ArcRotateCamera('camera', Math.PI / 4, Math.PI / 4, 5, Vector3.Zero(), scene)
+
+    camera.lowerBetaLimit = 0.05
+    camera.upperBetaLimit = Math.PI / 2 - 0.05
+    camera.useFramingBehavior = true
+    if (camera.framingBehavior) {
+      camera.framingBehavior.mode = FramingBehavior.FitFrustumSidesMode
+      camera.framingBehavior.radiusScale = 1
+      camera.framingBehavior.framingTime = 0
     }
 
-    _canvas = document.createElement('canvas')
-    _canvas.style.visibility = 'hidden'
-    _canvas.width = 256
-    _canvas.height = 256
-    document.body.appendChild(_canvas)
+    const light = new HemisphericLight('light', new Vector3(0, 1, 0), scene)
 
-    _engine = new Engine(_canvas, true)
-    _scene = new Scene(_engine)
-    _scene.clearColor = new Color4(0, 0, 0, 0)
+    function generateThumbnail(modelFile: ModelFile) {
+      return queue.add(async () => {
+        // 清掉前一個模型
+        if (lastContainer) {
+          lastContainer.removeAllFromScene()
+          lastContainer.dispose()
+          lastContainer = undefined
+        }
 
-    _camera = new ArcRotateCamera('camera', Math.PI / 4, Math.PI / 4, 5, Vector3.Zero(), _scene)
+        const container = await LoadAssetContainerAsync(modelFile.file, scene, {
+          pluginOptions: {
+            gltf: {
+              async preprocessUrlAsync(url) {
+                const file = await pipe(
+                  modelFile.path.replace(modelFile.name, ''),
+                  (path) => getFileFromPath(rootFsHandle, `${path}${url}`),
+                )
 
-    _camera.lowerBetaLimit = 0.05
-    _camera.upperBetaLimit = Math.PI / 2 - 0.05
-    _camera.useFramingBehavior = true
-    if (_camera.framingBehavior) {
-      _camera.framingBehavior.mode = FramingBehavior.FitFrustumSidesMode
-      _camera.framingBehavior.radiusScale = 1
-      _camera.framingBehavior.framingTime = 0
+                const blobUrl = URL.createObjectURL(file)
+                blobUrlList.push(blobUrl)
+
+                return blobUrl
+              },
+            },
+          },
+        })
+        lastContainer = container
+        container.addAllToScene()
+
+        const meshes = container.meshes.filter((m) => m.getTotalVertices() > 0)
+        camera.framingBehavior?.zoomOnMeshesHierarchy(meshes, true)
+
+        engine.runRenderLoop(() => {
+          scene.render()
+        })
+        await scene.whenReadyAsync(true)
+
+        camera.beta = Math.PI / 4
+        camera.alpha = Math.PI / 4
+
+        const base64 = await Tools.CreateScreenshotAsync(engine, camera, {
+          width: 256,
+          height: 256,
+        })
+        engine.stopRenderLoop()
+
+        const res = await fetch(base64)
+        return await res.blob()
+      })
     }
 
-    const light = new HemisphericLight('light', new Vector3(0, 1, 0), _scene)
+    function dispose() {
+      canvas.remove()
+      engine.dispose()
+    }
+
+    return {
+      generateThumbnail,
+      dispose,
+    }
   }
 
   function generateThumbnail(modelFile: ModelFile) {
-    return queue.add(async () => {
-      init()
+    const generator = generatorList[index]
+    if (!generator) {
+      throw new Error('Generator not found')
+    }
 
-      const [engine, scene, camera] = [_engine, _scene, _camera]
-      if (!engine || !scene || !camera) {
-        throw new Error('Engine or Scene or Camera not initialized')
-      }
-
-      // 清掉前一個模型
-      if (lastContainer) {
-        lastContainer.removeAllFromScene()
-        lastContainer.dispose()
-        lastContainer = undefined
-      }
-
-      const container = await LoadAssetContainerAsync(modelFile.file, scene, {
-        pluginOptions: {
-          gltf: {
-            async preprocessUrlAsync(url) {
-              const file = await pipe(
-                modelFile.path.replace(modelFile.name, ''),
-                (path) => getFileFromPath(rootFsHandle, `${path}${url}`),
-              )
-
-              const blobUrl = URL.createObjectURL(file)
-              blobUrlList.push(blobUrl)
-
-              return blobUrl
-            },
-          },
-        },
-      })
-      lastContainer = container
-      container.addAllToScene()
-
-      const meshes = container.meshes.filter((m) => m.getTotalVertices() > 0)
-      camera.framingBehavior?.zoomOnMeshesHierarchy(meshes, true)
-
-      engine.runRenderLoop(() => {
-        scene.render()
-      })
-      await scene.whenReadyAsync(true)
-
-      camera.beta = Math.PI / 4
-      camera.alpha = Math.PI / 4
-
-      const base64 = await Tools.CreateScreenshotAsync(engine, camera, {
-        width: 256,
-        height: 256,
-      })
-      engine.stopRenderLoop()
-
-      const res = await fetch(base64)
-      return await res.blob()
-    })
+    index = (index + 1) % concurrency
+    return generator.generateThumbnail(modelFile)
   }
 
   return { generateThumbnail }
