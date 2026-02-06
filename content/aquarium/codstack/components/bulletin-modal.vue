12ef0344a7b407a8bf3fa948e648823a05389726
Codfisher <pony2140@livemail.tw>
2026-02-06 11:08:49 +0800

    feat

--- content/aquarium/codstack/components/bulletin-modal.vue
+++ content/aquarium/codstack/components/bulletin-modal.vue
@@ -60,9 +60,9 @@ const { locale, t } = useSimpleI18n({
     intro: [
       '<div class="font-bold text-2xl mb-6">歡迎來到 CodStack！⁑｡٩(ˊᗜˋ*)و✧⁕</div>',
       '自<a href="https://youtu.be/PPWyUhT6gRk?si=XVD7uTdHl3Mr9kFU" target="_blank">派對動物</a>之後，我一直想繼續做其他有趣的 3D 專案，但 3D 建模實在是太難了，程式還沒開始寫，搞建模就搞到熱情冷卻了。゜・(PД`q｡)・゜',
-      '直到某天發現原來有現成的模型套組，感謝 <a href="https://kenney.nl/assets" target="_blank">Kenny</a>、<a href="https://kaylousberg.itch.io" target="_blank">Kay</a> 這些佛心大神們提供免費的 3D 模型。，否則我自己畫，等到真的做出來，應該是 10 年後了。( ´•̥̥̥ ω •̥̥̥` )',
+      '直到某天發現原來有現成的模型包，感謝 <a href="https://kenney.nl/assets" target="_blank">Kenny</a>、<a href="https://kaylousberg.itch.io" target="_blank">Kay</a>、<a href="https://tinytreats.itch.io/" target="_blank">Isa</a> 這些佛心大神們提供免費的 3D 模型包，否則我自己畫，等到真的做出來，應該是 10 年後了。( ´•̥̥̥ ω •̥̥̥` )',
       '<span></span>',
-      '不過使用這些 3D Kit 時有些小困擾，沒辦法快速預覽內容就算了，用程式碼組裝實在是極度痛苦。(›´ω`‹ )',
+      '不過使用這些 3D Pack 時有些小困擾，沒辦法快速預覽內容就算了，用程式碼組裝實在是極度痛苦。(›´ω`‹ )',
       '先在 3D 軟體中組裝，又會失去在程式中控制的彈性，<span class="font-bold">於是 CodStack 誕生了！(/≧▽≦)/</span>',
       '<span></span>',
       'CodStack 使用 <a href="https://www.babylonjs.com/" target="_blank">babylon.js</a>、<a href="https://vuejs.org/" target="_blank">Vue</a> 與 <a href="https://ui.nuxt.com/" target="_blank">Nuxt UI</a> 開發，目標是讓組裝 3D 模型變得像拼積木一樣簡單，可輸出結構資料並再次匯入編輯，方便 3D 網頁專案載入模型，免安裝，趕快來試試看吧。',
